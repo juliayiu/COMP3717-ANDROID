bea63e6d4aa1133b4c90da9162d6467aa54394f7
JULIAYIU <jyiu93@gmail.com>
2015-01-20 17:05:45 -0800

    hi

--- app/src/main/java/com/example/julia/android3/MainActivity.java
+++ app/src/main/java/com/example/julia/android3/MainActivity.java
@@ -40,6 +40,6 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void foo(final View view){
-        Toast.makeText(getApplication(), "wut", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplication(), "JULIA WAS HERE", Toast.LENGTH_LONG).show();
     }
 }
