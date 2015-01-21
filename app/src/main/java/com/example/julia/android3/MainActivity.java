4ae9579b0415a590d2561d4dad721e2988d8ac1e
JULIAYIU <jyiu93@gmail.com>
2015-01-20 22:20:05 -0800

    pushpush

--- app/src/main/java/com/example/julia/android3/MainActivity.java
+++ app/src/main/java/com/example/julia/android3/MainActivity.java
@@ -6,6 +6,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+import android.app.SearchManager;
+import android.content.Context;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.widget.SearchView;
+
+
 
 
 public class MainActivity extends ActionBarActivity {
@@ -21,6 +28,14 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+
+
         return true;
     }
 
@@ -37,6 +52,27 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class SearchResultsActivity extends Activity {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            handleIntent(getIntent());
+        }
+
+        @Override
+        protected void onNewIntent(Intent intent) {
+            handleIntent(intent);
+        }
+
+        private void handleIntent(Intent intent) {
+
+            if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+                String query = intent.getStringExtra(SearchManager.QUERY);
+                //use the query to search your data somehow
+            }
+        }
     }
 
     public void foo(final View view){
