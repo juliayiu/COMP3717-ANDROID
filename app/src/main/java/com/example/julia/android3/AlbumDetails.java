d406d98dd1ab876dc9dc8e6449f5a9f2b8b42364
TheLawrence <lawsarmiento@gmail.com>
2015-01-28 19:45:04 -0800

    meuh done.

--- app/src/main/java/com/example/julia/android3/AlbumDetails.java
+++ app/src/main/java/com/example/julia/android3/AlbumDetails.java
@@ -33,11 +33,12 @@ public class AlbumDetails extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        /*
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
+        */
         return super.onOptionsItemSelected(item);
     }
 
