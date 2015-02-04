ca9f023c89ece605dca2f84432df7ce6d9f4e398
JULIAYIU <jyiu93@gmail.com>
2015-02-04 02:19:44 -0800

    julias work as of feb 4 night

--- app/src/main/java/com/example/julia/android3/PhotoDetails.java
+++ app/src/main/java/com/example/julia/android3/PhotoDetails.java
@@ -1,5 +1,6 @@
 package com.example.julia.android3;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +13,11 @@ public class PhotoDetails extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_details);
+
+        // View Pager
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        ImageAdapter adapter = new ImageAdapter(this);
+        viewPager.setAdapter(adapter);
     }
 
 
